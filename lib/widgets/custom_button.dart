681c9ec851ea4067abdd2f8057531b9a7d0b0605
Anmol <sainianmol1507@gmail.com>
2024-02-08 22:37:42 +0530

    Upload custom widgets

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.onTap,
+    required this.text,
+  }) : super(key: key);
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(double.infinity, 40),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+
+      ),
+      onPressed: onTap,
+      child: Text(text),
+    );
+  }
+}
