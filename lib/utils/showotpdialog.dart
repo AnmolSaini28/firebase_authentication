5ca994add51e50b0d631159fc5597f41c8b8402a
Anmol <sainianmol1507@gmail.com>
2024-02-10 13:16:43 +0530

    Upload utils

---
+++ lib/utils/showotpdialog.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: sort_child_properties_last
+import 'package:flutter/material.dart';
+
+void showOTPDialog({
+  required BuildContext context,
+  required TextEditingController codeController,
+  required VoidCallback onPressed,
+}) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) => AlertDialog(
+      title: const Text("Enter OTP"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextField(
+            controller: codeController,
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text("Done"),
+          onPressed: onPressed,
+        )
+      ],
+    ),
+  );
+}
